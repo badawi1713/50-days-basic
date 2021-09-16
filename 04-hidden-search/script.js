40c6bf2aedc66d28856077648dce282ff7c8f0b4
badawi1713 <dzaky.badawi@gmail.com>
2021-09-16 10:42:39 +0700

    add day-3 project hidden search

---
+++ 04-hidden-search/script.js
@@ -0,0 +1,9 @@
+// script
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
